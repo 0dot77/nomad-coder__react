27bed1cfb7ddf73e678c705b52de31aa4208f1eb
0dot77 <yty0706@gmail.com>
2022-04-03 09:30:24 +0900

    todos

--- cra-example-2/src/App.js
+++ cra-example-2/src/App.js
@@ -1,12 +1,44 @@
 import { useState } from "react";
-import Hello from "./Hello.js";
 
 function App() {
-  const [showing, setShowing] = useState(false);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={() => setShowing((prev) => !prev)}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {toDos.length}</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (toDo === "") {
+            return;
+          }
+          setTodo(""); // submit 할 때마다 input field 초기화
+          setToDos((currentArray) => [toDo, ...currentArray]);
+          /* 
+           !: setToDos의 사용법
+           setTodos(currentArray) {
+             !: 여기서 toDo의 값은 인풋 필드에 입력되는 값이다.
+            return [toDo, ...currentArray];
+           }
+          */
+        }}
+      >
+        <input
+          value={toDo}
+          onChange={(event) => {
+            setTodo(event.target.value);
+          }}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add to do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
